2e752adf5863c7feed4fbb887439818b93ba2b31
Anouk Ruhaak <anoukruhaak@gmail.com>
2016-12-12 16:13:04 +0100

    update mapbox

--- app/src/main/java/com/hkw/arrivinginberlin/LanguageSettingFragment.java
+++ app/src/main/java/com/hkw/arrivinginberlin/LanguageSettingFragment.java
@@ -175,6 +175,7 @@ public class LanguageSettingFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
